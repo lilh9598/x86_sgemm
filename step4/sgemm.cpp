414ec9c585378c3b51d1c8ee4fe69f5323b323e5
lilianhuang <lilianhuang@bytedance.com>
2021-09-30 10:48:36 +0800

    step4 packb only

--- step4/sgemm.cpp
+++ step4/sgemm.cpp
@@ -4,54 +4,28 @@
 
 void kernel_8x8(int k, const float *packa, int lda, const float *b, int ldb, float *c, int ldc);
 
-void mcxkc_sgemm(int m, int n, int k, const float *packa, int lda, const float *packb, int ldb, float *c, int ldc);
+void mcxkc_sgemm(int m, int n, int k, const float *a, int lda, const float *packb, int ldb, float *c, int ldc);
 
 void packB_Kcxn(const float *b, int ldb, int k, int nr, float *packb);
 
-void packA_KcxMc(const float *a, int lda, int k, int mr,float *packa);
-
 void my_sgemm(int m, int n, int k, const float *a, int lda, const float *b, int ldb, float *c, int ldc) {
     float *packb = new float[ROUND_UP(n, 8) * Kc];
-    float *packa = new float[ROUND_UP(Mc, 8) * Kc];
 
     for (int p = 0; p < k; p += Kc) {
         int pb = min(Kc, k - p);
 
         for (int j = 0; j < n; j += 8) {
-            packB_Kcxn(&B(p, j), ldb, pb, min(n - j, 8),packb + j * pb);
+            packB_Kcxn(&B(p, j), ldb, pb, min(n - j, 8), packb + j * pb);
         }
 
         for (int i = 0; i < m; i += Mc) {
             int ib = min(Mc, m - i);
 
-            for (int ii = 0; ii < ib; ii += 8) {
-                packA_KcxMc(&A(ii + i, p), lda, pb, min(ib - ii, 8),packa + ii * pb);
-            }
-
-            mcxkc_sgemm(ib, n, pb, packa, lda, packb, ldb, &C(i, 0), ldc);
+            mcxkc_sgemm(ib, n, pb, &A(i, p), lda, packb, ldb, &C(i, 0), ldc);
         }
     }
 
     delete[] packb;
-    delete[] packa;
-}
-
-void packA_KcxMc(const float *a, int lda, int k, int mr, float *packa) {
-    const float *pack_ptr[8];
-    for (int i = 0; i < mr; i++) {
-        pack_ptr[i] = a + i;
-    }
-
-    for (int i = mr; i < 8; i++) {
-        pack_ptr[i] = a;
-    }
-
-    for (int p = 0; p < k; p++) {
-        for (int i = 0; i < 8; i++) {
-            *packa++ = *pack_ptr[i];
-            pack_ptr[i] += lda;
-        }
-    }
 }
 
 void packB_Kcxn(const float *b, int ldb, int k, int nr, float *packb) {
@@ -69,10 +43,10 @@ void packB_Kcxn(const float *b, int ldb, int k, int nr, float *packb) {
     }
 }
 
-void mcxkc_sgemm(int m, int n, int k, const float *packa, int lda, const float *packb, int ldb, float *c, int ldc) {
+void mcxkc_sgemm(int m, int n, int k, const float *a, int lda, const float *packb, int ldb, float *c, int ldc) {
     for (int j = 0; j < n; j += 8) {
         for (int i = 0; i < m; i += 8) {
-            kernel_8x8(k, packa + i * k, lda, packb + j * k, ldb, &C(i, j), ldc);
+            kernel_8x8(k, &A(i, 0), lda, packb + j * k, ldb, &C(i, j), ldc);
         }
     }
 }
@@ -92,13 +66,13 @@ void kernel_8x8(int k, const float *a, int lda, const float *b, int ldb, float *
     ymm6 = _mm256_loadu_ps(&C(0, 6));
     ymm7 = _mm256_loadu_ps(&C(0, 7));
 
-#define FMA(x)                            \
+#define FMA(x)                         \
     ymm8 = _mm256_broadcast_ss(b + x); \
     ymm##x = _mm256_fmadd_ps(ymm8, ymm9, ymm##x);
 
     // dot 8 x 8
     for (int p = 0; p < k; p++) {
-        ymm9 = _mm256_loadu_ps(a);
+        ymm9 = _mm256_loadu_ps(&A(0, p));
         FMA(0)
         FMA(1)
         FMA(2)
@@ -108,7 +82,6 @@ void kernel_8x8(int k, const float *a, int lda, const float *b, int ldb, float *
         FMA(6)
         FMA(7)
         b += 8;
-        a += 8;
     }
 
     _mm256_storeu_ps(&C(0, 0), ymm0);
